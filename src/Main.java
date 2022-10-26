d74a70a133a7116653a5a332a350d68969b521b5
Drfox81 <Drfox11181@mail.ru>
2022-10-27 01:06:21 +0300

    Дополнительно попробовал создать Класс методов (утилити класс или как там его)

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,22 @@
 public class Main {
     public static void main(String[] args) {
         Author first = new Author("Толстой", "Лев");
-        System.out.println("Автор первой книги - " + first.getNameAuthorFirst() + " " + first.getNameAuthorSecond());
         Book bookOne = new Book(first, "Война и мир", 2010);
-        System.out.println("Год публикации - " + bookOne.getYearPublication());
-        bookOne.setYearPublication(2012);
-        System.out.println("Год публикации новый - " + bookOne.getYearPublication());
-        System.out.println("Фамилия автора - " + bookOne.getNameAuthor().getNameAuthorFirst());
-        System.out.println("Имя автора - " + bookOne.getNameAuthor().getNameAuthorSecond());
-        System.out.println();
-
         Author second = new Author("Горький", "Максим");
-        System.out.println("Автор второй книги -  " + second.getNameAuthorFirst() + " " + second.getNameAuthorSecond());
         Book bookTwo = new Book(second, "Вий", 1998);
-    }
+        Book bookThree = new Book(first, "Война и мир", 2010);
+        System.out.println(bookOne.toString());
+        System.out.println(bookTwo.toString());
+        bookTwo.setYearPublication(2020);
+        System.out.println(bookTwo.toString());
 
+        BookServis bookServis = new BookServis();
+        System.out.println(bookServis.toString(bookOne));
+        System.out.println("Результат сравнения книг - " + bookOne.equals(bookThree));
+        System.out.println("Результат сравнения авторов книг - " + first.equals(second));
+        System.out.println(first.hashCode() + " || " + second.hashCode());
+        System.out.println(bookOne.hashCode() + " || " + bookThree.hashCode());
+
+
+    }
 }
